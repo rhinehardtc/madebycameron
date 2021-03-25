1119744e50720cbcf81cf33ff9e4b702b5d75bbf
rhinehardtc <rhinehardtc@gmail.com>
2021-03-25 13:27:47 -0500

    added mask divs

--- madebycameron/src/ProjectsContainer.js
+++ madebycameron/src/ProjectsContainer.js
@@ -1,9 +1,12 @@
 
 const ProjectsContainer = (props) => {
+
+    const maskStyle = {backgroundColor: "#00ffff47", width: "100%",height: "15%"};
+
     return(
-        <div style={{display: "flex", height: "80vh"}}>
-            <div></div>
-            <div></div>
+        <div style={{display: "flex", flexFlow: "column",width: "100%", height: "85vh"}}>
+            <div style={maskStyle}></div>
+            <div style={maskStyle}></div>
         </div>
     );
 }
